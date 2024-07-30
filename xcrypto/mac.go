db26f71129ab4a4157a0de10694ad13967346a73
sanmuyan <m@sanmuyan.com>
2024-07-30 17:34:50 +0800

    v0.1.22

--- xcrypto/mac.go
+++ xcrypto/mac.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 )
 
+// GenerateHmacSha256 生成 hmac sha256
 func GenerateHmacSha256(message string, key string) string {
 	h := hmac.New(sha256.New, []byte(key))
 	h.Write([]byte(message))
