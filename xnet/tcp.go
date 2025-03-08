1a5e00204ddac41640ad56dd91969a7bc6e2b994
sanmuyan <m@sanmuyan.com>
2025-03-09 00:57:38 +0800

    v0.1.25

--- xnet/tcp.go
+++ xnet/tcp.go
@@ -4,44 +4,36 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/binary"
-	"github.com/sanmuyan/xpkg/xconstant"
+	"io"
 )
 
 // Encode 用于 TCP 包编码
 func Encode(data []byte) ([]byte, error) {
-	length := int32(len(data))
+	length := uint32(len(data))
 	pkg := new(bytes.Buffer)
-	err := binary.Write(pkg, binary.LittleEndian, length)
-	if err != nil {
+
+	if err := binary.Write(pkg, binary.LittleEndian, length); err != nil {
 		return nil, err
 	}
-	err = binary.Write(pkg, binary.LittleEndian, data)
-	if err != nil {
-		return nil, err
-	}
+	pkg.Write(data)
 	return pkg.Bytes(), nil
 }
 
 // Decode 用于 TCP 包解码
 func Decode(reader *bufio.Reader) ([]byte, error) {
-	lengthByte, err := reader.Peek(4)
+	lengthByte := make([]byte, 4)
+	_, err := io.ReadFull(reader, lengthByte)
 	if err != nil {
 		return nil, err
-	}
-	lengthBuf := bytes.NewBuffer(lengthByte)
-	var length int32
-	err = binary.Read(lengthBuf, binary.LittleEndian, &length)
-	if err != nil {
-		return nil, err
-	}
-	if int32(reader.Buffered()) < length+4 {
-		return nil, xconstant.BufferedTooSmallError
 	}
 
-	pkg := make([]byte, int(4+length))
-	_, err = reader.Read(pkg)
+	length := binary.LittleEndian.Uint32(lengthByte)
+
+	pkg := make([]byte, length)
+	_, err = io.ReadFull(reader, pkg)
 	if err != nil {
 		return nil, err
 	}
-	return pkg[4:], nil
+
+	return pkg, nil
 }
