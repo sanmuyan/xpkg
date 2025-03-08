3249965718425086f46dedc7c66eb5dbe5bdde59
sanmuyan <m@sanmuyan.com>
2025-03-09 03:06:50 +0800

    v0.1.29

--- xnet/data.go
+++ xnet/data.go
@@ -2,18 +2,34 @@ package xnet
 
 import (
 	"fmt"
+	"strings"
 )
 
 func GetDataSpeed(b, s int) string {
 	bit := float64(b * 8 / s)
 	if bit >= 1024 && bit < 1024*1024 {
-		return fmt.Sprintf("%.2fKb/s", bit/1024)
+		f := fmt.Sprintf("%.2fKb/s", bit/1024)
+		sl := strings.Split(f, ".")
+		if sl[1] == "00Kb/s" {
+			return fmt.Sprintf("%.0fKb/s", bit/1024)
+		}
+		return f
 	}
 	if bit >= 1024*1024 && bit < 1024*1024*1024 {
-		return fmt.Sprintf("%.2fMb/s", bit/(1024*1024))
+		f := fmt.Sprintf("%.2fMb/s", bit/(1024*1024))
+		sl := strings.Split(f, ".")
+		if sl[1] == "00Mb/s" {
+			return fmt.Sprintf("%.0fMb/s", bit/(1024*1024))
+		}
+		return f
 	}
 	if bit >= 1024*1024*1024 {
-		return fmt.Sprintf("%.2fGb/s", bit/(1024*1024*1024))
+		f := fmt.Sprintf("%.2fMb/s", bit/(1024*1024*1024))
+		sl := strings.Split(f, ".")
+		if sl[1] == "00Mb/s" {
+			return fmt.Sprintf("%.0fGb/s", bit/(1024*1024*1024))
+		}
+		return f
 	}
 	return fmt.Sprintf("%.0fb/s", bit)
 }
--- xnet/data_test.go
+++ xnet/data_test.go
@@ -8,13 +8,16 @@ func TestGetDataSpeed(t *testing.T) {
 	if GetDataSpeed(1300, 1) != "10.16Kb/s" {
 		t.Error("GetDataSpeed() error")
 	}
-	if GetDataSpeed(1024, 1) != "8.00Kb/s" {
+	if GetDataSpeed(1024, 1) != "8Kb/s" {
 		t.Error("GetDataSpeed() error")
 	}
-	if GetDataSpeed(1024*1024, 1) != "8.00Mb/s" {
+	if GetDataSpeed(1024*1024, 1) != "8Mb/s" {
 		t.Error("GetDataSpeed() error")
 	}
-	if GetDataSpeed(1024*1024*1024, 1) != "8.00Gb/s" {
+	if GetDataSpeed(1024*1300, 1) != "10.16Mb/s" {
+		t.Error("GetDataSpeed() error")
+	}
+	if GetDataSpeed(1024*1024*1024, 1) != "8Gb/s" {
 		t.Error("GetDataSpeed() error")
 	}
 }
