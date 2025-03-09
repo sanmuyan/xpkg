789c2ae68908e7aa91eea0a16244a6870bcd07c4
sanmuyan <m@sanmuyan.com>
2025-03-09 14:43:05 +0800

    v0.1.33

--- xnet/data_test.go
+++ xnet/data_test.go
@@ -8,16 +8,16 @@ func TestGetDataSpeed(t *testing.T) {
 	if GetDataSpeed(1300, 1) != "10.16Kb/s" {
 		t.Error("GetDataSpeed() error")
 	}
-	if GetDataSpeed(1024, 1) != "8Kb/s" {
+	if GetDataSpeed(1024, 1) != "8.00Kb/s" {
 		t.Error("GetDataSpeed() error")
 	}
-	if GetDataSpeed(1024*1024, 1) != "8Mb/s" {
+	if GetDataSpeed(1024*1024, 1) != "8.00Mb/s" {
 		t.Error("GetDataSpeed() error")
 	}
 	if GetDataSpeed(1024*1300, 1) != "10.16Mb/s" {
 		t.Error("GetDataSpeed() error")
 	}
-	if GetDataSpeed(1024*1024*1024, 1) != "8Gb/s" {
+	if GetDataSpeed(1024*1024*1024, 1) != "8.00Gb/s" {
 		t.Error("GetDataSpeed() error")
 	}
 }
