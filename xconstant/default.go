caaa5ea36b0300db7640a75be6dcd1abdd749a9f
sanmuyan <m@sanmuyan.com>
2025-03-26 19:04:00 +0800

    v0.1.35

---
+++ xconstant/default.go
@@ -0,0 +1,9 @@
+package xconstant
+
+// 定义默认值
+
+// 定义默认常量
+const (
+	// PackageMaxLength 网络数据包允许的最大长度
+	PackageMaxLength = 1024 * 1024
+)
--- xconstant/errors.go
+++ xconstant/errors.go
@@ -11,10 +11,3 @@ var (
 	BadParameter          = errors.New("bad parameter")
 	PackageTooLengthError = errors.New("package too length")
 )
-
-// 常量定义
-
-const (
-	// PackageMaxLength 网络数据包允许的最大长度
-	PackageMaxLength = 1024 * 1024
-)
