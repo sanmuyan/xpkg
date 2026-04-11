b86fb4d37da599aeab8c6c50f3829353be8ee4b5
sanmuyan <m@sanmuyan.com>
2026-04-11 16:21:20 +0800

    v0.1.64

--- xjwt/jwt.go
+++ xjwt/jwt.go
@@ -6,67 +6,84 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-// CreateToken 创建 JWT，密钥长度不能小于32
-func CreateToken(claims jwt.Claims, key []byte) (string, error) {
-	if len(key) < 32 {
-		return "", errors.New("key length invalid")
+// ValidJwtKey 校验 JWT 密钥长度是否符合要求
+func ValidJWTKey(key []byte) error {
+	switch len(key) {
+	case 32:
+		return nil
+	default:
+		return errors.New("key length invalid")
+	}
+}
+
+// CreateToken 创建 JWT
+func CreateToken(claims jwt.Claims, key []byte) (tokenStr string, err error) {
+	err = ValidJWTKey(key)
+	if err != nil {
+		return
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenStr, err := token.SignedString(key)
+	tokenStr, err = token.SignedString(key)
 	if err != nil {
-		return tokenStr, err
+		return
 	}
-	return tokenStr, nil
+	return
 }
 
 // ParseToken 解析 JWT
-func ParseToken(tokenStr string, key []byte, claims jwt.Claims) (*jwt.Token, error) {
-	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (any, error) {
+func ParseToken(tokenStr string, key []byte, claims jwt.Claims) (token *jwt.Token, err error) {
+	err = ValidJWTKey(key)
+	if err != nil {
+		return
+	}
+	token, err = jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (any, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, errors.New("unexpected method")
 		}
 		return key, nil
 	})
 	if err != nil {
-		return token, err
+		return
 	}
 	if !token.Valid {
-		return token, errors.New("invalid token")
+		err = errors.New("token is invalid")
+		return
 	}
-	return token, nil
+	return
 }
 
 // CreateTokenRSA 创建 JWT，需要提供 PEM 私钥
-func CreateTokenRSA(claims jwt.Claims, privateKey []byte) (string, error) {
+func CreateTokenRSA(claims jwt.Claims, privateKey []byte) (tokenStr string, err error) {
 	pk, err := jwt.ParseRSAPrivateKeyFromPEM(privateKey)
 	if err != nil {
-		return "", err
+		return
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
-	tokenStr, err := token.SignedString(pk)
+	tokenStr, err = token.SignedString(pk)
 	if err != nil {
-		return tokenStr, err
+		return
 	}
-	return tokenStr, nil
+	return
 }
 
 // ParseTokenRSA 解析 JWT，需要提供 PEM 公钥
-func ParseTokenRSA(tokenStr string, publicKey []byte, claims jwt.Claims) (*jwt.Token, error) {
+func ParseTokenRSA(tokenStr string, publicKey []byte, claims jwt.Claims) (token *jwt.Token, err error) {
 	pk, err := jwt.ParseRSAPublicKeyFromPEM(publicKey)
 	if err != nil {
-		return nil, err
+		return
 	}
-	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (any, error) {
+	token, err = jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (any, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, errors.New("unexpected method")
 		}
 		return pk, nil
 	})
 	if err != nil {
-		return token, err
+		return
 	}
 	if !token.Valid {
-		return token, errors.New("invalid token")
+		err = errors.New("invalid token")
+		return
 	}
-	return token, nil
+	return
 }
