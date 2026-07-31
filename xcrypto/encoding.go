25fd6e0e59439f30afa0db52a7a80ef34c0c892c
sanmuyan <m@sanmuyan.com>
2026-07-31 15:30:59 +0800

    v0.1.66

--- xcrypto/encoding.go
+++ xcrypto/encoding.go
@@ -1,6 +1,9 @@
 package xcrypto
 
-import "encoding/base64"
+import (
+	"encoding/base64"
+	"encoding/hex"
+)
 
 func Base64Encode(src []byte) []byte {
 	return base64.StdEncoding.AppendEncode(nil, src)
@@ -8,4 +11,12 @@ func Base64Encode(src []byte) []byte {
 
 func Base64Decode(src []byte) ([]byte, error) {
 	return base64.StdEncoding.AppendDecode(nil, src)
+}
+
+func HexEncode(src []byte) []byte {
+	return hex.AppendEncode(nil, src)
+}
+
+func HexDecode(src []byte) ([]byte, error) {
+	return hex.AppendDecode(nil, src)
 }
